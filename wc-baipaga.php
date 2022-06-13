df977ee8d67a1ae0cef3e3bdfc00104d6469f546
baipaga <baipaga@ps611076.dreamhostps.com>
2022-06-13 11:26:53 -0700

    Initial repo commit.

---
+++ wc-baipaga.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Woocommerce Bai Pagamentos
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     Integração Woocommerce e gateway BAI.
+ * Author:          WP Helpers
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     wc-baipaga
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wc_Baipaga
+ */
+
+// Your code starts here.
